d59b8c78541184e551670eb4b664c8724ab8a736
anil-g11h <anilraj659@gmail.com>
2026-02-23 13:26:25 +0530

    feat: add scroll to top functionality on route change

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -50,6 +50,10 @@ function Layout({ children }: { children: React.ReactNode }) {
         };
     }, []);
 
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+    }, [location.pathname]);
+
     return (
         <div>
             {shouldShowBackButton ? (
